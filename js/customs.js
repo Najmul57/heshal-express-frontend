ae31a399ce51090fd68785a78127afa072881f33
najmul <najmulcse2@gmail.com>
2022-06-04 17:03:12 +0600

    others page design

--- js/customs.js
+++ js/customs.js
@@ -2,28 +2,50 @@
     "use strict";
 
 
- // owlCarousel
- $('.review_active').owlCarousel({
-    loop: true,
-    margin: 0,
-    items: 1,
-    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-    nav: false,
-    dots: true,
-    autoplay: true,
-    smartSpeed: 1000,
-    responsive: {
-        0: {
-            items: 1
-        },
-        767: {
-            items: 1
-        },
-        992: {
-            items: 1
+    // Partnet Active
+    $('.partner_active').owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        nav: false,
+        dots: true,
+        autoplay: true,
+        smartSpeed: 5000,
+        responsive: {
+            0: {
+                items: 2
+            },
+            767: {
+                items: 4
+            },
+            992: {
+                items: 5
+            }
         }
-    }
-})
+    })
+    // Review Active
+    $('.review_active').owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        nav: false,
+        dots: true,
+        autoplay: true,
+        smartSpeed: 3000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            767: {
+                items: 1
+            },
+            992: {
+                items: 1
+            }
+        }
+    })
 
 
 })(jQuery);
